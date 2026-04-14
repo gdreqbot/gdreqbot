ed99adc9628ea1a8191a0c74b4c78dd24fa08205
GalaxyVinci05 <vincenzopaolo0@gmail.com>
2026-04-14 15:45:18 +0200

    update client version and downloads

--- src/config.ts
+++ src/config.ts
@@ -5,8 +5,8 @@ export default {
     ownerId: process.env.OWNER_ID,
     prefix: process.env.PREFIX,
     clientVersion: {
-        win32: "1.1.0",
-        darwin: "1.1.0",
-        linux: "1.1.0"
+        win32: "1.1.1",
+        darwin: "1.1.1",
+        linux: "1.1.1"
     }
 };
