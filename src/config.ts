1aee22e94de88423d122c257185b04f7abc195cc
GalaxyVinci05 <vincenzopaolo0@gmail.com>
2026-04-18 15:33:14 +0200

    bump client versions

--- src/config.ts
+++ src/config.ts
@@ -5,8 +5,8 @@ export default {
     ownerId: process.env.OWNER_ID,
     prefix: process.env.PREFIX,
     clientVersion: {
-        win32: "1.2.0",
-        darwin: "1.2.0",
-        linux: "1.2.0"
+        win32: "1.2.1",
+        darwin: "1.2.1",
+        linux: "1.2.1"
     }
 };
